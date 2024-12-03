4a8ab8977fa78bb41712c08559c5b68b42ad5904
legoChoi <chlx159@gmail.com>
2024-12-03 23:07:35 +0900

    예외 기본 - 체크와 언체크의 활용

---
+++ src/test/java/hello/jdbc/exception/basic/UnCheckedAppTest.java
@@ -0,0 +1,83 @@
+package hello.jdbc.exception.basic;
+
+import lombok.extern.slf4j.Slf4j;
+import org.assertj.core.api.Assertions;
+import org.junit.jupiter.api.Test;
+
+import java.net.ConnectException;
+import java.sql.SQLException;
+
+@Slf4j
+public class UnCheckedAppTest {
+
+    @Test
+    void unChecked() {
+        Controller controller = new Controller();
+        Assertions.assertThatThrownBy(controller::request)
+                .isInstanceOf(RuntimeSQLException.class);
+    }
+
+    @Test
+    void printEx() {
+        Controller controller = new Controller();
+        try {
+            controller.request();
+        } catch (Exception e) {
+            log.info("ex", e);
+        }
+    }
+
+    static class Controller {
+        Service service = new Service();
+
+        public void request() {
+            service.logic();
+        }
+    }
+
+    static class Service {
+        Repository repository = new Repository();
+        NetworkClient networkClient = new NetworkClient();
+
+        public void logic() {
+            repository.call();
+            networkClient.call();
+        }
+    }
+
+    static class NetworkClient {
+        public void call() {
+            throw new RuntimeConnectException("연결 실패");
+        }
+    }
+
+    static class Repository {
+        public void call() {
+            try {
+                runSQL();
+            } catch (SQLException e) { // 체크 예외를 받아서
+                // 이전 예외를 알 수 있게 같이 넣어주자 : 예외 포함
+                throw new RuntimeSQLException(e); // 런타임 예외로 던져버리자
+            }
+        }
+
+        public void runSQL() throws SQLException {
+            throw new SQLException("ex");
+        }
+    }
+
+    static class RuntimeConnectException extends RuntimeException {
+        public RuntimeConnectException(String message) {
+            super(message);
+        }
+    }
+
+    static class RuntimeSQLException extends RuntimeException {
+        public RuntimeSQLException() {
+        }
+
+        public RuntimeSQLException(Throwable cause) {
+            super(cause);
+        }
+    }
+}
