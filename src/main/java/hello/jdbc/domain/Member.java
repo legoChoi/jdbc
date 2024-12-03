7f8ba767d78aae014569b0fbacdc59ca048ce4ab
legoChoi <chlx159@gmail.com>
2024-12-03 16:32:41 +0900

    JDBC - 회원 조회

--- src/main/java/hello/jdbc/domain/Member.java
+++ src/main/java/hello/jdbc/domain/Member.java
@@ -1,9 +1,12 @@
 package hello.jdbc.domain;
 
+import lombok.EqualsAndHashCode;
 import lombok.Getter;
 import lombok.Setter;
+import lombok.ToString;
 
 @Getter @Setter
+@ToString @EqualsAndHashCode
 public class Member {
 
     private String memberId;
--- src/main/java/hello/jdbc/repository/MemberRepositoryV0.java
+++ src/main/java/hello/jdbc/repository/MemberRepositoryV0.java
@@ -5,6 +5,7 @@ import hello.jdbc.domain.Member;
 import lombok.extern.slf4j.Slf4j;
 
 import java.sql.*;
+import java.util.NoSuchElementException;
 
 /**
  *  Ver.0
@@ -35,6 +36,35 @@ public class MemberRepositoryV0 {
             throw e;
         } finally { // 리소스 반환
             close(conn, pstmt, null);
+        }
+    }
+
+    public Member findById(String memberId) throws SQLException {
+        String sql = "select * from member where member_id = ?";
+
+        Connection conn = null;
+        PreparedStatement pstmt = null;
+        ResultSet rs = null;
+
+        try {
+            conn = getConnection();
+            pstmt = conn.prepareStatement(sql);
+            pstmt.setString(1, memberId);
+            rs = pstmt.executeQuery();
+
+            if (rs.next()) { // 최초의 커서는 데이터를 가지고 있지 않음
+                Member member = new Member();
+                member.setMemberId(rs.getString("member_id"));
+                member.setMoney(rs.getInt("money"));
+                return member;
+            } else {
+                throw new NoSuchElementException("member not found memberId = " + memberId);
+            }
+        } catch (SQLException e) {
+            log.error("db error", e);
+            throw e;
+        } finally {
+            close(conn, pstmt, rs);
         }
     }
 
